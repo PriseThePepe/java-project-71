d8574a668217fc6c81851e3996ba1bbb39b0dc8b
Ivan Piglov <famenko72@gmail.com>
2024-05-14 21:51:01 +0300

    add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,10 @@ application{
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation ("info.picocli:picocli:4.7.6")
 }
+
 
 tasks.test {
     useJUnitPlatform()
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(description = "Compares two configuration files and shows a difference.",
+        name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0")
+public class App implements Runnable {
+
     public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
+    }
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
 }
