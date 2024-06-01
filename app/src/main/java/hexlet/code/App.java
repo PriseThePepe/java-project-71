e7dcfccd7eb7f239d19fc5c2246a10900fbf39b2
Ivan Piglov <famenko72@gmail.com>
2024-06-01 20:03:38 +0300

    implement comparison of flat Json files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,26 +5,33 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
+import java.util.concurrent.Callable;
 
 @Command(description = "Compares two configuration files and shows a difference.",
         name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0")
-public class App implements Runnable {
+public class App implements Callable<String> {
     @Option(names = {"-f","--format"},paramLabel = "format",description = "output format [default: stylish]")
     private String format = "stylish";
 
     @Parameters(index = "0",paramLabel = "filepath1", description = "path to first file")
-    private File filepath1;
+    private String filepath1;
 
     @Parameters(index = "1",paramLabel = "filepath2", description = "path to second file")
-    private File filepath2;
+    private String filepath2;
 
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
     }
     @Override
-    public void run() {
-        System.out.println("Hello World!");
+    public String call() {
+        try {
+            System.out.println(Differ.generate(filepath1,filepath2));
+            return Differ.generate(filepath1,filepath2);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+
     }
+
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,60 @@
 package hexlet.code;
 
-public class Differ {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Differ {
+    public static String generate(String firstFilepath, String secondFilepath) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Path firstPath = Paths.get(firstFilepath).toAbsolutePath();
+        Path secondPath = Paths.get(secondFilepath).toAbsolutePath();
+
+        File firstJson = new File(String.valueOf(firstPath));
+        File secondJson = new File(String.valueOf(secondPath));
+
+        Map<String, String> jsonMap1 = objectMapper.readValue(firstJson, new TypeReference<Map<String, String>>() {
+        });
+        Map<String, String> jsonMap2 = objectMapper.readValue(secondJson, new TypeReference<Map<String, String>>() {
+        });
+
+        return getJsonDiff(jsonMap1, jsonMap2);
+    }
+
+    private static String getJsonDiff(Map<String, String> jsonMap1, Map<String, String> jsonMap2) {
+        StringBuilder builder = new StringBuilder();
+
+        List<String> sortedList = new ArrayList<>(jsonMap1.keySet());
+        sortedList.addAll(jsonMap2.keySet());
+
+        sortedList = sortedList.stream()
+                .distinct()
+                .sorted()
+                .toList();
+        for (String key : sortedList) {
+            String value1 = jsonMap1.get(key);
+            String value2 = jsonMap2.get(key);
+
+            if (value1 == null) {
+                builder.append("+ ").append(key).append(": ").append(value2).append(System.lineSeparator());
+            } else if (value2 == null) {
+                builder.append("- ").append(key).append(": ").append(value1).append(System.lineSeparator());
+            } else if (value1.equals(value2)) {
+                builder.append("  ").append(key).append(": ").append(value1).append(System.lineSeparator());
+            } else {
+                builder.append("- ").append(key).append(": ").append(value1).append(System.lineSeparator());
+                builder.append("+ ").append(key).append(": ").append(value2).append(System.lineSeparator());
+            }
+        }
+        return builder.toString();
+    }
 }
