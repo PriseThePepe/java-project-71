1efc800a6dfa3f0056e25c616be9d30a2f5f739a
Ivan Piglov <famenko72@gmail.com>
2024-06-05 14:04:39 +0300

    fix build.gradle/kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     application
@@ -26,5 +29,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 tasks.jacocoTestReport { reports { xml.required.set(true) } }
