36ecc458c058621a8ccece8e0f80a56c25e4225e
Ivan Piglov <famenko72@gmail.com>
2024-07-05 14:06:22 +0300

    add formatters

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -1,4 +1,19 @@
 package hexlet.code.formatters;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.Collection;
+import java.util.Map;
+
 public class JsonFormatter {
+    public static String jsonFormat(Map<String, Map<String, Object>> diffMap) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        Collection<Map<String, Object>> diffValues = diffMap.values();
+
+        return  objectMapper.writeValueAsString(diffValues);
+    }
 }
--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -1,4 +1,55 @@
 package hexlet.code.formatters;
 
+import java.util.List;
+import java.util.Map;
+
 public class PlainFormatter {
+    public static String plainFormat(Map<String, Map<String, Object>> diffMap) {
+        StringBuilder builder = new StringBuilder();
+
+        for (Map<String, Object> diff : diffMap.values()) {
+            String status = (String) diff.get("status");
+
+            switch (status) {
+                case "removed" ->
+                        builder.append("Property ").
+                                append("'").
+                                append(diff.get("key")).
+                                append("'").
+                                append(" was removed").
+                                append(System.lineSeparator());
+                case "added" ->
+                        builder.append("Property ").
+                                append("'").
+                                append(diff.get("key")).
+                                append("'").
+                                append(" was added with value: ").
+                                append(formatValue(diff.get("new value"))).
+                                append(System.lineSeparator());
+                case "unchanged" -> {
+                }
+                default -> builder.append("Property ").
+                        append("'").
+                        append(diff.get("key")).
+                        append("'").
+                        append(" was updated.").
+                        append(" From ").
+                        append(formatValue(diff.get("old value"))).
+                        append(" to ").
+                        append(formatValue(diff.get("new value"))).
+                        append(System.lineSeparator());
+            }
+        }
+
+        return builder.toString().trim();
+    }
+    private static String formatValue(Object value) {
+        if (value instanceof Map || value instanceof List) {
+            return "[complex value]";
+        }
+        if (value instanceof String) {
+            return "'" + value + "'";
+        }
+        return String.valueOf(value);
+    }
 }
