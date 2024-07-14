ace8c2914e7d35942fd3a8b08ad516238c9115da
Ivan Piglov <famenko72@gmail.com>
2024-07-14 18:28:20 +0300

    fix parser

--- app/src/main/java/hexlet/code/Parser/Parser.java
+++ app/src/main/java/hexlet/code/Parser/Parser.java
@@ -5,36 +5,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parse(String filepath) throws IOException {
-        Path path = Paths.get(filepath).toAbsolutePath();
-
-        File file = new File(String.valueOf(path));
+    public static Map<String, Object> parse(String content, String mapperType) throws IOException {
 
         Map<String, Object> parsedMap = null;
 
-        if (getFileExtension(filepath).equals(".json")) {
+        if (mapperType.equals("json")) {
             ObjectMapper objectMapper = new JsonMapper();
 
-            parsedMap = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {
+            parsedMap = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
             });
-        } else if (getFileExtension(filepath).equals(".yml")) {
+        } else if (mapperType.equals("yml")) {
             ObjectMapper yamlMapper = new YAMLMapper();
 
-            parsedMap = yamlMapper.readValue(file, new TypeReference<Map<String, Object>>() {
+            parsedMap = yamlMapper.readValue(content, new TypeReference<Map<String, Object>>() {
             });
         }
         return parsedMap;
     }
-    private static String getFileExtension(String filepath) {
-        int index = filepath.indexOf('.');
-        return index == -1 ? ".none" : filepath.substring(index);
-    }
+
 }
 
